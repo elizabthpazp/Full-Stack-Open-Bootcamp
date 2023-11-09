a994aba1461b7b454f57e1154e7b4829b300521b
elizabthpazp <elizabthpazp695@gmail.com>
2023-11-08 22:13:06 -0500

    exercises 1.12.-1.14

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,94 +1,38 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-const StatisticLine = (props) => {
-  return (
-    <tr>{props.text ? props.text:null}:<td>{props.value}</td></tr>
-  );
-};
-
-const Statistics = (props) => {
-  if (props.good[1] !== 0 || props.neutral[1] !== 0 || props.bad[1] !== 0) {
-    return (
-      <div>
-        <h1>{props.title}</h1>
-        <table>
-          <tbody> 
-            <StatisticLine
-              text={props.good[0]}
-              value={props.good[1]}
-            ></StatisticLine>
-            <StatisticLine
-              text={props.neutral[0]}
-              value={props.neutral[1]}
-            ></StatisticLine>
-            <StatisticLine
-              text={props.bad[0]}
-              value={props.bad[1]}
-            ></StatisticLine>
-            <StatisticLine
-              text={props.all[0]}
-              value={props.bad[1] + props.good[1] + props.neutral[1]}
-            ></StatisticLine>
-            <StatisticLine
-              text={props.average}
-              value={(props.bad[1] + props.good[1] + props.neutral[1]) / 3}
-            ></StatisticLine>
-            <StatisticLine
-              text={props.positive}
-              value={(props.good[1] / 3) * 100 + "%"}
-            ></StatisticLine>
-          </tbody>
-        </table>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <h1>{props.title}</h1>
-        <p>{props.hidden}</p>
-      </div>
-    );
-  }
-};
-
-const App = () => {
-  // save clicks of each button to its own state
-  let [good, setGood] = useState(0);
-  let [neutral, setNeutral] = useState(0);
-  let [bad, setBad] = useState(0);
-
-  const comments = {
-    title: "Give Feedback",
-    good: ["Good", good],
-    neutral: ["Neutral", neutral],
-    bad: ["Bad", bad],
-    all: "All",
-    average: "Average",
-    positive: "Positive",
-    statistics: "Statistics",
-    hidden: "No Feedback Given",
-  };
+const App = (props) => {
+  let [selected, setSelected] = useState(0)
+  const titleBtn = "Next Anecdote", titleVote = "vote" , most = "Anecdote with most votes" 
+  let points = [ 1, 3, 4, 7, 4, 8] 
+  let [copy, setCopy] = useState([...points] )
+  const getAleatory = () =>{ 
+    return Math.floor(Math.random() * props.anecdotes.length);
+  }  
+  const getPos =()=> { return Math.max(copy) }
   return (
     <div>
-      <h1>{comments.title}</h1>
-      <button onClick={() => setGood(good++)}>{comments.good[0]}</button>
-      <button onClick={() => setNeutral(neutral++)}>
-        {comments.neutral[0]}
-      </button>
-      <button onClick={() => setBad(bad++)}>{comments.bad[0]}</button>
-      <Statistics
-        hidden={comments.hidden}
-        title={comments.statistics}
-        good={comments.good}
-        neutral={comments.neutral}
-        bad={comments.bad}
-        all={comments.all}
-        average={comments.average}
-        positive={comments.positive}
-      />
+      <h2>{props.anecdotes[selected]}</h2>
+      <h3>has {copy[selected]} votes</h3>
+      <button onClick={()=> setCopy(copy = copy[selected] += 1)}>{titleVote}</button>
+      <button onClick={()=> setSelected(selected = getAleatory())}>{titleBtn}</button>
+      <h2>{most}</h2>
+      <h3>{props.anecdotes[getPos()]}</h3>
+      <h3>has {copy[getPos()]} votes</h3>
     </div>
-  );
-};
+  )
+}
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
